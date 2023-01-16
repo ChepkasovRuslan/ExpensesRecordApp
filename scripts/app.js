7f10f56654c9f0c32b72c79946752258a7b8351e
Ruslan Chepkasov <chepkasov.itrum@gmail.com>
2023-01-16 14:43:25 +0300

    now total sum on back-end

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,4 @@
-const URL = 'http://localhost:8000/expenses/';
+const URL = 'http://localhost:8000';
 
 let expenses = [];
 
@@ -8,7 +8,7 @@ window.onload = async () => {
 
 const getAllExpenses = async () => {
   try {
-    const response = await fetch(URL, {
+    const response = await fetch(URL + '/expenses', {
       method: 'GET'
     });
 
@@ -18,13 +18,16 @@ const getAllExpenses = async () => {
   }
 }
 
-const calcTotalSum = (expenses) => {
+const calcTotalSum = async () => {
   try {
-    return expenses.reduce((prev, curr) => {
-      return prev + curr.sum;
-    }, 0);
+    const sum = await fetch(URL + '/expenses/sum', {
+      method: 'GET'
+    }).then(response => response.json())
+      .then(sum => sum.totalSum);
+
+    return sum;
   } catch (error) {
-    return -1;
+    return undefined;
   }
 }
 
@@ -32,5 +35,5 @@ const render = async () => {
   expenses = await getAllExpenses();
 
   const totalSum = document.getElementById('total-sum');
-  totalSum.innerHTML = `Итого: ${calcTotalSum(expenses)} р.`;
+  totalSum.innerHTML = `Итого: ${await calcTotalSum()} р.`;
 }
